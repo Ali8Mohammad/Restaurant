85738775555225eb62353c4efb5b8cda86f4e05c
unknown <alim94258@gmail.com>
2024-05-03 20:35:39 +0300

    update

---
+++ Assets/js/main.js
@@ -0,0 +1,8 @@
+const darkMode = document.querySelector('.fa-moon');
+darkMode.addEventListener('click', () => {
+   document.body.classList.toggle("dark-mode");
+});
+const darkMode1 = document.querySelector('.li-moon');
+darkMode1.addEventListener('click', () => {
+    document.body.classList.toggle("dark-mode"); 
+})
